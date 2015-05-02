e213651882dbe7390bdd1737662a716ef42bde33
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-02 16:47:44 -0400

    Progress on Erlang, can get first half of string

--- palindrome.erl
+++ palindrome.erl
@@ -11,18 +11,21 @@
 -export([display/1]).
 -export([perms/1]).
 
+% Taken from http://www.erlang.org/doc/programming_examples/list_comprehensions.html
 perms([]) -> [[]];
 perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
 
 isPalindrome(InputString) ->
-  FirstHalf = substr(InputString, len(InputString)/2).
-  SecondHalf = substr(InputString, len(InputString)/2, (len(InputString)/2)-1).
+  FirstHalf  = string:substr(InputString, 1, trunc(string:len(InputString) / 2)),
+  SecondHalf = string:substr(InputString, trunc(string:len(InputString) / 2)),
+  io:format("First half is ~s~n", [FirstHalf]),
+  io:format("Second half is ~s~n", [SecondHalf]),
   true.
 
 display(InputString) ->
-  % InputIsPalindrome = isPalindrome(InputString).
+  InputIsPalindrome = isPalindrome(InputString),
   % if
   %   InputIsPalindrome =:= true -> io:format("~s is a palindrome", [InputString]);
   %   true -> io:format("nope", []);
   % end.
-  io:format("I am a palindrome").
+  io:format("I am a palindrome~n").
