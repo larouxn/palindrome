fc2ca8c25fe9da5f6c7f76024d68fecf217dad48
larouxn <larouxn@gmail.com>
2015-05-02 19:02:11 -0400

    Fixed inconsistency in messages

--- palindrome.rb
+++ palindrome.rb
@@ -4,7 +4,7 @@ end
 if word == word.reverse
   puts "String given is already a palindrome."
 elsif freq_hash.values.select {|num| num.odd?}.length > 1
-  puts "String cannot be made into a palindrome."
+  puts "String given cannot be made into a palindrome."
 else
   freq_hash.each_pair {|key, value| palindrome = key and value - 1 if value.odd? } and freq_hash.each_pair do |key, value|
     palindrome = key * (value / 2) + palindrome + key * (value / 2)
