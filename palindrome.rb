91be2a434145f87c170c9a8532c678548e206f96
larouxn <larouxn@gmail.com>
2015-05-02 14:32:51 -0400

    Tidying up, 11 lines baby, woo

--- palindrome.rb
+++ palindrome.rb
@@ -11,7 +11,6 @@
 freq_hash = {} and palindrome = "" and word = gets.chomp and letters = word.split('') and letters.each do |letter|
   freq_hash.has_key?(letter) ? freq_hash[letter] += 1 : freq_hash[letter] = 1
 end
-
 if word == word.reverse
   puts "String given is already a palindrome."
 elsif freq_hash.values.select {|num| num.odd?}.length > 1
@@ -19,6 +18,5 @@ elsif freq_hash.values.select {|num| num.odd?}.length > 1
 else
   freq_hash.each_pair {|key, value| palindrome = key and value - 1 if value.odd? } and freq_hash.each_pair do |key, value|
     palindrome = key * (value / 2) + palindrome + key * (value / 2)
-  end
-  puts "Palindrome: #{palindrome}"
+  end and puts "Palindrome: #{palindrome}"
 end
