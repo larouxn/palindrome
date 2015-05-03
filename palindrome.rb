4f90366380fd237c7eb01d88b81b4e34fc4b797d
larouxn <larouxn@gmail.com>
2015-05-02 23:18:05 -0400

    Added a linebreak for readability

--- palindrome.rb
+++ palindrome.rb
@@ -1,6 +1,7 @@
 freq_hash = {} and palindrome = "" and word = gets.chomp and letters = word.split('') and letters.each do |letter|
   freq_hash.has_key?(letter) ? freq_hash[letter] += 1 : freq_hash[letter] = 1
 end
+
 if word == word.reverse
   puts "String given is already a palindrome."
 elsif freq_hash.values.select {|num| num.odd?}.length > 1
