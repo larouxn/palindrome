f99b987564b11001c95bd8a4cdb3bbb6b39adc16
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-04 13:59:37 -0400

    Shortened Scala

--- palindrome.scala
+++ palindrome.scala
@@ -1,5 +1,4 @@
-def isPalindrome(ls: List[Char]): Boolean =
-  (ls take (ls.length / 2)) == ((ls takeRight (ls.length / 2)).reverse)
+def isPalindrome(ls: List[Char]): Boolean = ls == ls.reverse
 
 def canBePalindrome(ls: List[Char]): (Boolean, List[Char]) = {
   var canBe = false
@@ -16,6 +15,6 @@ def canBePalindrome(ls: List[Char]): (Boolean, List[Char]) = {
 println(
   if (isPalindrome(args(0).toList)) args(0) + " is a palindrome"
   else "A palindromic form of " + args(0) + " is " + (
-          if (!canBePalindrome(args(0).toList)._1) "impossible"
-          else (canBePalindrome(args(0).toList)._2).mkString)
+    if (!canBePalindrome(args(0).toList)._1) "impossible"
+    else (canBePalindrome(args(0).toList)._2).mkString)
 )
