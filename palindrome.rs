e4f527815c396d66ad52a31b73048cda8def08b7
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-04 13:52:26 -0400

    Shortened Erlang, removed Rust executable

--- palindrome.rs
+++ palindrome.rs
@@ -1,7 +1,6 @@
 use std::env;
 
-fn is_palindrome(input: &str) -> bool {
-    let mut ch_vec: Vec<&str> = input.split("").collect();
+fn is_palindrome(ch_vec: Vec<&str>) -> bool {
     let ch_vec_2 = ch_vec.to_vec();
     ch_vec.reverse();
     return (ch_vec == ch_vec_2);
@@ -10,6 +9,16 @@ fn is_palindrome(input: &str) -> bool {
 fn main() {
     let args: Vec<_> = env::args().collect();
     let input = &args[1];
-    let is_pal = is_palindrome(input);
+    let ch_vec: Vec<&str> = input.split("").collect();
+    let is_pal = is_palindrome(ch_vec);
     println!("Is {} a palindrome? {}", input, is_pal);
+    // if is_pal {
+    //     for combo in ch_vec.permutations() {
+    //         if is_palindrome(combo) {
+    //             let combo_str =
+    //             println!("A palindromic form of {} is {}", input, combo);
+    //             break;
+    //         }
+    //     }
+    // }
 }
