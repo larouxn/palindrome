da245508f7423684062153cf7bfb41c1c7f3ff5b
larouxn <larouxn@gmail.com>
2015-05-07 15:55:21 -0400

    Cleanup

--- palindrome.exs
+++ palindrome.exs
@@ -1,8 +1,6 @@
 defmodule Palindrome do
   word = "racecar"
   letters = String.split(word, "", trim: true)
-  numberOfOdds = 0
-  freq = %{r: 2, a: 2, c: 2, e: 1}
 
   odd? = &(rem(&1, 2) != 0) # from Elixir's site | Enum.filter(1..3, odd?)
 
@@ -15,5 +13,5 @@ defmodule Palindrome do
   end
 
   List.foldl(letters, %{}, fn(x, acc) -> Map.update(acc, String.to_atom(x), 1, fn(val) -> val + 1 end) end)
-  
+
 end
