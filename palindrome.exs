3b6ce468bc09090371e241b60640b91755fa4042
larouxn <larouxn@gmail.com>
2015-05-07 17:12:00 -0400

    Completed can_be_palindrome(map) function

--- palindrome.exs
+++ palindrome.exs
@@ -1,10 +1,20 @@
 defmodule Palindrome do
-  word = "racecar"
-  letters = String.split(word, "", trim: true)
 
-  odd? = &(rem(&1, 2) != 0) # from Elixir's site | Enum.filter(1..3, odd?)
+  # Takes string, returns map with letters as keys and frequencies as values
+  defp count_letters(string) do
+    letters = String.split(string, "", trim: true)
+    List.foldl(letters, %{}, fn(x, acc) -> Map.update(acc, String.to_atom(x), 1, fn(val) -> val + 1 end) end)
+  end
 
-  def palindrome?(string) do
+  # Takes map, returns true if at most one letter exists in odd frequency
+  def can_be_palindrome(map) do
+    import Integer, only: [is_odd: 1]
+    values = Map.values(map)
+    odds = Enum.filter(values, fn(x) -> rem(x, 2) != 0 end)
+    if Enum.count(odds) <= 1, do: true, else: false
+  end
+
+  def is_palindrome(string) do
     if string == String.reverse(string) do
       IO.puts "#{string} is a palindrome."
     else
@@ -12,6 +22,9 @@ defmodule Palindrome do
     end
   end
 
-  List.foldl(letters, %{}, fn(x, acc) -> Map.update(acc, String.to_atom(x), 1, fn(val) -> val + 1 end) end)
+  # def create(map) do
+  # end
 
+  # def main(string) do
+  # end
 end
