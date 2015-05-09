1ec696a14f5d58edf53a828bf309a30ed4b74d13
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-09 13:15:58 -0400

    Starting on Maps for Kotlin, but they're immutable

--- palindrome.kt
+++ palindrome.kt
@@ -13,10 +13,18 @@ fun main(args: Array<String>) {
       else                    map.put(c, map.get(c)+1)
     }
 
-    if (map.count( {entry -> (entry.value % 2 != 0)} ) % 2 == 0) {
+    if (map.count( {entry -> (entry.value % 2 != 0)} ) > 1) {
       println(args[0] + " cannot be made into a palindrome")
     } else {
-      // do other stuff
+      val builtString = ""
+      val oddCountMap  = map.filter( {entry -> (entry.value % 2 != 0)} )
+      val evenCountMap = map.filterNot( {entry -> (entry.value % 2 != 0)} )
+
+      //println(buildString)*/
+      // 1) if there is one, decrement count of odd-#'d char and put it in a string
+      // 2) make a string out of n/2 instances of the even-#'d characters, add it to
+      // the beginning
+      // 3) reverse that string and add it to the end
     }
   }
 }
