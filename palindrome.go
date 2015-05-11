e3c26ce88e8a7a393edc44f79282800a697f800d
larouxn <larouxn@gmail.com>
2015-05-11 14:06:57 -0400

    First third of Go completed, checks if string is already a palindrome

--- palindrome.go
+++ palindrome.go
@@ -2,8 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-  var message string = "Hello world!\n"
+var word string = "racecar"
 
-  fmt.Printf(message)
+// Borrowed from Russ Cox | http://goo.gl/XrHvrk | slightly modified
+func reverse(input string) string {
+    // Get Unicode code points.
+    n := 0
+    rune := make([]rune, len(input))
+    for _, r := range input {
+        rune[n] = r
+        n++
+    }
+    rune = rune[0:n]
+    // Reverse
+    for i := 0; i < n/2; i++ {
+        rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+    }
+    // Convert back to UTF-8.
+    output := string(rune)
+    return output
+}
+
+
+func is_palindrome(x string) bool {
+    if x == reverse(x) {
+        return true
+    } else {
+        return false
+    }
+}
+
+func main() {
+    fmt.Printf("%s is a palindrome: %t\n", word, is_palindrome(word))
 }
