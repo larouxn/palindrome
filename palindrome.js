6e62f12c8aaed00c20a98fa1d2418d3fa6dc13f6
larouxn <larouxn@gmail.com>
2015-05-12 01:14:31 -0400

    Completed second third of JavaScript, can now detect if string can be a palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,37 @@
+var word = "racecar",
+    freq = {}
+
 function reverse(string) {
   return string.split("").reverse().join("");
 }
 
 function isPalindrome(string) {
-  if(string == reverse(string)) {
-    return true
+  string == reverse(string) ? true : false
+}
+
+function canBePalindrome(string) {
+  var numberOfOdds = 0
+
+  for (letter of word.split("")) {
+    freq.hasOwnProperty(letter) ? freq[letter]++ : freq[letter] = 1
+  }
+  for (letter of Object.getOwnPropertyNames(freq)) {
+    if (freq[letter] % 2 != 0) numberOfOdds++
+  }
+
+  numberOfOdds > 1 ? false : true
+}
+
+// function createPalindrome(string) {
+//
+// }
+
+function main(string) {
+  if isPalindrome(string) {
+    string + "is a palindrome."
+  } else if !canBePalindrome(string ){
+    string + "cannot be made into a palindrome."
   } else {
-    return false
+    createPalindrome(string)
   }
 }
