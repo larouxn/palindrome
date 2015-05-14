9cf0e9ef1f0ab3e2fc6f47f701751553d1503956
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-13 23:37:01 -0400

    Kotlin lets me use Maps now!

--- palindrome.kt
+++ palindrome.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
   if (isPalindrome(chList))
     println(args[0] + " is a palindrome")
   else {
-    val map = hashMapOf<String, Int>()
+    var map = hashMapOf<String, Int>()
     for (c in chList) {
       if (map.get(c) == null) map.put(c, 1)
       else                    map.put(c, map.get(c)+1)
@@ -16,15 +16,20 @@ fun main(args: Array<String>) {
     if (map.count( {entry -> (entry.value % 2 != 0)} ) > 1) {
       println(args[0] + " cannot be made into a palindrome")
     } else {
-      val builtString = ""
-      val oddCountMap  = map.filter( {entry -> (entry.value % 2 != 0)} )
-      val evenCountMap = map.filterNot( {entry -> (entry.value % 2 != 0)} )
+      var builtString  = ""
+      var oddCountMap  = map.filter( {entry -> (entry.value % 2 != 0)} )
+      var evenCountMap = map.filterNot( {entry -> (entry.value % 2 != 0)} )
 
-      //println(buildString)*/
+      for (entry in evenCountMap) {
+        builtString += entry.key
+        map.put(entry.key, entry.value-1)
+      }
+
       // 1) if there is one, decrement count of odd-#'d char and put it in a string
       // 2) make a string out of n/2 instances of the even-#'d characters, add it to
       // the beginning
-      // 3) reverse that string and add it to the end
+      builtString += builtString.reverse()
+      println("A palindromic form of this word is " + builtString)
     }
   }
 }
