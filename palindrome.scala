54ac0da8c49dc98b08d785f56308bd40a25570f4
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-15 13:01:01 -0400

    Fixing Scala. Building palindrome only thing left

--- palindrome.scala
+++ palindrome.scala
@@ -1,20 +1,24 @@
 def isPalindrome(ls: List[Char]): Boolean = ls == ls.reverse
 
 def canBePalindrome(ls: List[Char]): (Boolean, List[Char]) = {
-  var canBe = false
-  var validPerm = List[Char]()
+  // Graciously borrowed from solution found on
+  // http://stackoverflow.com/questions/11448685/
+  //   scala-how-can-i-count-the-number-of-occurrences-in-a-list
+  val map = ls.groupBy(identity).mapValues(_.size)
 
-  for (perm <- ls.permutations.toArray; if !canBe) {
-    canBe = isPalindrome(perm)
-    if (canBe)
-      validPerm = perm
+  map count ((entry: (Char, Int)) => entry._2 % 2 == 0) match {
+    case n if n > 1 => (false, ls)
+    case _          => (true, ls)
+  }
+}
+
+
+if (isPalindrome(args(0).toList)) println(args(0) + " is a palindrome")
+else {
+  val finalStr = canBePalindrome(args(0).toList) match {
+    case (true, ls)  => ls.mkString
+    case _           => "impossible"
   }
 
-  (canBe, validPerm)
+  println("A palindromic form of " + args(0) + " is " + finalStr)
 }
-println(
-  if (isPalindrome(args(0).toList)) args(0) + " is a palindrome"
-  else "A palindromic form of " + args(0) + " is " + (
-    if (!canBePalindrome(args(0).toList)._1) "impossible"
-    else (canBePalindrome(args(0).toList)._2).mkString)
-)
