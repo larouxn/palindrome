3606c476f9a2aeb7b461afc864401089620fb233
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-15 21:04:50 -0400

    Scala fixed, basically stole Kotlin code lol

--- palindrome.scala
+++ palindrome.scala
@@ -1,14 +1,35 @@
 def isPalindrome(ls: List[Char]): Boolean = ls == ls.reverse
 
-def canBePalindrome(ls: List[Char]): (Boolean, List[Char]) = {
+def canBePalindrome(ls: List[Char]): (Boolean, String) = {
   // Graciously borrowed from solution found on
   // http://stackoverflow.com/questions/11448685/
   //   scala-how-can-i-count-the-number-of-occurrences-in-a-list
   val map = ls.groupBy(identity).mapValues(_.size)
 
-  map count ((entry: (Char, Int)) => entry._2 % 2 == 0) match {
-    case n if n > 1 => (false, ls)
-    case _          => (true, ls)
+  map count ((entry: (Char, Int)) => entry._2 % 2 != 0) match {
+    case n if n > 1 => (false, ls.mkString)
+    case _          => {
+      val oddsMap  = map filter ((entry: (Char, Int)) => entry._2 % 2 != 0)
+      val evensMap = map filter ((entry: (Char, Int)) => entry._2 % 2 == 0)
+      var buildStr = ""
+      for (entry <- evensMap) {
+        for (i <- 1 to entry._2 / 2) {
+          buildStr += entry._1
+        }
+      }
+
+      var single = '\u0000'
+      for (entry <- oddsMap) {
+        if (entry._2 > 1) {
+          for (i <- 1 to entry._2 / 2)
+            buildStr += entry._1
+        } else {
+          single = entry._1
+        }
+      }
+
+      (true, buildStr + single + buildStr.reverse)
+    }
   }
 }
 
