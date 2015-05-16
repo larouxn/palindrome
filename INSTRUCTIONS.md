ca87df80e48c952e752134372b984c07bd9fad2f
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-16 00:42:22 -0400

    Dumped most of Erlang code, added instructions

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,19 @@
+#Instructions to Run
+
+Erlang
+
+- Open REPL: `erl`
+
+- Compile: `c(palindrome).`
+
+- Run: `palindrome::display(INPUT_STRING).`
+
+Kotlin
+
+- Compile: `kotlin-jvm palindrome.kt -include-runtime -d palindrome.jar`
+
+- Run: `java -jar palindrome.jar INPUT_STRING`
+
+Scala
+
+- Run: `scala [-nocompdaemon] palindrome.scala INPUT_STRING`
--- palindrome.erl
+++ palindrome.erl
@@ -1,16 +1,9 @@
 -module(palindrome).
 -export([isPalindrome/1]).
 -export([display/1]).
--export([perms/1]).
 -export([findPalindrome/1]).
 
-% Taken from http://www.erlang.org/doc/programming_examples/list_comprehensions.html
-perms([]) -> [[]];
-perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
-
-isPalindrome(InputString) ->
-  Rev = lists:reverse(InputString),
-  InputString =:= Rev.
+isPalindrome(InputString) -> InputString =:= lists:reverse(InputString).
 
 findPalindrome(InputString) ->
   IsPal = isPalindrome(InputString),
@@ -19,17 +12,15 @@ findPalindrome(InputString) ->
     true  -> ""
   end.
 
+createPalindrome(InputString) ->
+  % do stuff
+  end.
+
 display(InputString) ->
   InputIsPalindrome = isPalindrome(InputString),
   if
     InputIsPalindrome =:= true ->
       io:format("~s is a palindrome!~n", [InputString]);
     true ->
-      InputPerms      = perms(InputString),
-      PermResults     = lists:map(fun palindrome:findPalindrome/1, InputPerms),
-      ResultsNoBlanks = lists:filter(fun (Result) -> Result =/= [] end, PermResults),
-      case ResultsNoBlanks of
-        [] -> io:format("A palindromic form of ~s is impossible~n", [InputString]);
-        _  -> io:format("A palindromic form of ~s is ~s~n", [InputString, hd(ResultsNoBlanks)])
-      end
+      createPalindrome(InputString)
   end.
