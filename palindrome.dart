31916895ce57d7cedd297c9f19b17ec9372d837d
larouxn <larouxn@gmail.com>
2015-05-16 21:28:34 -0400

    Completed second third of Dart, can now check if string can be a palindrome

--- palindrome.dart
+++ palindrome.dart
@@ -1,15 +1,59 @@
 import 'dart:io';
 
-isPalindrome(String input) {
-  if (input == input.split('').reversed.join('')) {
-    print('$input is a palindrome.');
-  } else {
-    print('$input is not a palindrome.');
-  }
+Map countLetters(String string) {
+    var freq  = new Map();
+    for (var letter in string.split('')) {
+        if (freq.containsKey(letter)) {
+            ++freq[letter];
+        } else {
+            freq[letter] = 1;
+        }
+    }
+    return freq;
 }
 
-void main() {
-  stdout.writeln('Enter a word or phrase');
-  String input = stdin.readLineSync();
-  isPalindrome(input);
+Bool isPalindrome(String input) {
+    return (input == input.split('').reversed.join(''))
+        ? true
+        : false;
 }
+
+Bool canBePalindrome(Map freq) {
+    var numberOfOdds = 0;
+    for (var amount in freq.values) {
+        if (amount % 2 != 0) {
+            ++numberOfOdds;
+        }
+    }
+    return (numberOfOdds <= 1)
+        ? true
+        : false;
+}
+
+// String createPalindrome(Map freq) {
+//
+// }
+
+void main() {
+    stdout.writeln("Enter a word or phrase");
+    String input = stdin.readLineSync();
+    Map freq = countLetters(input);
+    if (canBePalindrome(freq)) {
+        print("Can totally be a pally.");
+    } else {
+        print("Cannot be a pally.");
+    }
+}
+
+// void main() {
+//     stdout.writeln("Enter a word or phrase");
+//     String input = stdin.readLineSync();
+//     Map freq = countLetters(input);
+//     if (isPalindrome(input)) {
+//         print("$input is already a palindrome.");
+//     } else if (canBePalindrome(freq)) {
+//         print("$input cannot be made into a palindrome.");
+//     } else {
+//         print("Palindrome of $string: ${Palindrome.create(freq)}");
+//     }
+// }
