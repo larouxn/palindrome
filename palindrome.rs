b1c5680ab81ed4a58afa5f36c474a928772ba105
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-17 20:50:55 -0400

    Part 1 and frequency counting in Rust complete

--- palindrome.rs
+++ palindrome.rs
@@ -1,14 +1,27 @@
 use std::env;
-//use std::collections::HashMap;
+use std::collections::HashMap;
 
 fn is_palindrome(input: &str) -> bool {
-    let forward: Vec<&str> = input.split("").collect();
-    let reverse: Vec<&str> = input.rsplit("").collect();
+    let forward: Vec<char> = input.chars().collect();
+    let mut reverse: Vec<char> = input.chars().collect();
+    reverse.reverse();
     forward == reverse
 }
 
 fn build_palindrome(input: &str) {
-    println!("can't do it son");
+    let split: Vec<char> = input.chars().collect();
+    println!("{}", split.len());
+    let mut map: HashMap<&char, i32> = HashMap::new();
+    for ch in split.iter() {
+        if !map.contains_key(ch) {
+            map.insert(ch, 1);
+        } else if let Some(n) = map.get_mut(ch) {
+            *n = *n + 1;
+        }
+    }
+    for (k, v) in map.iter() {
+        println!("key = {}, val = {}", *k, *v);
+    }
 }
 
 fn main() {
