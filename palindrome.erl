ba5d14d3088a3d1214e341e0095b152be5e502e3
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-18 17:12:13 -0400

    Massive progress on Erlang: part 2 of 3 done

--- palindrome.erl
+++ palindrome.erl
@@ -1,17 +1,34 @@
 -module(palindrome).
 -export([isPalindrome/1]).
 -export([display/1]).
--export([createPalindrome/1]).
+-export([checkIfCanBuildPalindrome/1]).
+-export([buildPalindrome/3]).
 
 isPalindrome(InputString) ->
   InputString =:= lists:reverse(InputString).
 
-createPalindrome(InputString) ->
-  io:format("dingo").
+buildPalindrome(EvensMap, OddsMap, InputString) ->
+  InputString.
+
+checkIfCanBuildPalindrome(InputString) ->
+  UpdateFun = fun(Old)    -> Old + 1 end,
+  FoldFun   = fun(C, Map) -> dict:update(C, UpdateFun, 1, Map) end,
+  OccurrencesMap = lists:foldl(FoldFun, dict:new(), InputString),
+  EvensMap = dict:filter(fun(K, V) -> V rem 2 == 0 end, OccurrencesMap),
+  OddsMap  = dict:filter(fun(K, V) -> V rem 2 /= 0 end, OccurrencesMap),
+  OddsSize = dict:size(OddsMap),
+  if
+    OddsSize > 1 ->
+      io:format("~s cannot be made into a palindrome~n", [InputString]);
+    true ->
+      io:format("A palindromic form of ~s is ~s~n", [InputString, buildPalindrome(EvensMap, OddsMap, InputString)])
+  end.
 
 display(InputString) ->
-  InputIsPalindrome = isPalindrome(InputString),
+  IsPalindrome = isPalindrome(InputString),
   if
-    InputIsPalindrome =:= true -> io:format("~s is a palindrome!~n", [InputString]);
-    true                       -> createPalindrome(InputString)
+    IsPalindrome ->
+      io:format("~s is a palindrome!~n", [InputString]);
+    true ->
+      checkIfCanBuildPalindrome(InputString)
   end.
