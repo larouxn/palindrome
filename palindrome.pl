5ead0646e7c7a129ca743a2d7017ea245c9b82de
Yuxrier <john.sullivan8@marist.edu>
2015-05-21 06:18:35 -0400

    actually finished part 2 and started part 3

--- palindrome.pl
+++ palindrome.pl
@@ -1,8 +1,10 @@
 :- dynamic letterFreq/2
-palindrome(X):- accRev(X,[],Y), X == Y, findall(Z,letterFreq(_,Z),List), modCount(List, 0, Count).
+palindrome(X):- accRev(X,[],Y), X == Y.
+palindrome(X):- findall(Z,letterFreq(_,Z),List), modCount(List, 0, Count), Count < 2, newPalindrome(List, [], NewDrome), write(NewDrome).
 accRev([H|T],A,R):- accRev(T,[H|A],R), letterFreq(H,X), Y is X + 1, assert(letterFreq(H,Y)).
 accRev([H|T],A,R):- accRev(T,[H|A],R), assert(letterFreq(H,0)).
 accRev([],A,A).
 modCount([H|T],A,R):- 1 is mod(H,2), B is A + 1, modCount(T,B,R).
 modCount([H|T],A,R):- modCount(T,A,R).
 modCount([],A,A).
+newPalindrome([H|T],A,R):- %to do
