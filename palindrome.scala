a0a194248c754226b8f9d372111fab53f9583e7f
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-23 00:26:37 -0400

    Little baby changes to Scala

--- palindrome.scala
+++ palindrome.scala
@@ -1,11 +1,10 @@
-def isPalindrome(ls: List[Char]): Boolean = ls == ls.reverse
+def isPalindrome(ls: List[Char]) = ls == ls.reverse
 
 def canBePalindrome(ls: List[Char]): (Boolean, String) = {
   // Graciously borrowed from solution found on
   // http://stackoverflow.com/questions/11448685/
   //   scala-how-can-i-count-the-number-of-occurrences-in-a-list
   val map = ls.groupBy(identity).mapValues(_.size)
-
   map count ((entry: (Char, Int)) => entry._2 % 2 != 0) match {
     case n if n > 1 => (false, ls.mkString)
     case _          => {
@@ -33,13 +32,11 @@ def canBePalindrome(ls: List[Char]): (Boolean, String) = {
   }
 }
 
-
 if (isPalindrome(args(0).toList)) println(args(0) + " is a palindrome")
 else {
   val finalStr = canBePalindrome(args(0).toList) match {
     case (true, ls)  => ls.mkString
     case _           => "impossible"
   }
-
   println("A palindromic form of " + args(0) + " is " + finalStr)
 }
