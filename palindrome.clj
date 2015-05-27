e0ac3ec3f222d70b050e375a16cbc864450e3d10
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-05-26 23:12:45 -0400

    Part 2 of Clojure done. This lang is so elegant

--- palindrome.clj
+++ palindrome.clj
@@ -1,3 +1,18 @@
+(def orig (first *command-line-args*))
+
 (def input (seq (char-array (first *command-line-args*))))
+
 (defn palindrome? [s] (= s (reverse s)))
-(printf "%s%n" (palindrome? input))
+
+(def occurrences-map (frequencies input))
+
+(def evens-map (filter (fn [[_, v]] (even? v)) occurrences-map))
+
+(def odds-map (filter (fn [[_, v]] (odd? v)) occurrences-map))
+
+(if (palindrome? input)
+  (printf "%s is a palindrome%n" orig)
+  (if (> (count (keys odds-map)) 1)
+    (printf "%s cannot be made into a palindrome%n" orig)
+    (println "dingo"))
+)
